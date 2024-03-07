a0b56f3b983d8f19621aab5154d8579c517191c5
NaveenReddy0427 <naveendeva909@gmail.com>
2024-03-07 08:53:15 +0530

    adding comments to the server

--- server.js
+++ server.js
@@ -3,10 +3,15 @@
 // require http module/library
 const http = require("http");
 
+// created server
 const server = http.createServer((req, res)=>{
+
+    // here comes teh response
     res.end("Hello macha!")
 })
 
+
+// server is listening through port 3000 for clients request
 server.listen(3000, ()=>{
     console.log("server is listening on port: 3000")
 })
