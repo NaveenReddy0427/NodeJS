ad4506ec99fde9e6c3a81c38231380537e8a9c8d
NaveenReddy0427 <naveendeva909@gmail.com>
2024-03-07 11:57:02 +0530

    changed to mjs file then only ES6 module will work, in this file we write ES6 export syntax

---
+++ modules/file1.mjs
@@ -0,0 +1,25 @@
+// modules are useful to get the data of one javascript to the another javascript file
+// two ways to write modules 1) common-js module 2) ES6 module
+
+
+// ES6 syntax
+export const sum = (x, y) => {
+  return x + y;
+};
+
+
+export const name = "Naveen"
+
+
+function sub(x, y){
+    return x-y;
+}
+
+
+
+
+// commonjs module
+// module.exports = {
+//     add: sum,
+// }
+
